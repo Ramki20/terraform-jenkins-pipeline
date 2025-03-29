f987c7b6e34699e22615f6dba5c35c990fbf6e63
Ramki20 <127626883+Ramki20@users.noreply.github.com>
2025-03-29 09:57:19 -0500

    Update provider.tf-region-coalesce(var.aws_region, env.AWS_DEFAULT_REGION)

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region     = coalesce(var.aws_region, env.AWS_DEFAULT_REGION)
 }
