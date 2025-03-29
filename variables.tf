1efb5ca8e232d91c229d7768d61cacddf5747b25
Ramki20 <127626883+Ramki20@users.noreply.github.com>
2025-03-29 10:26:06 -0500

    fix invalid ami id

--- variables.tf
+++ variables.tf
@@ -16,11 +16,10 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-0c7217cdde317cfec"
 }
 
 variable "instance_type" {
